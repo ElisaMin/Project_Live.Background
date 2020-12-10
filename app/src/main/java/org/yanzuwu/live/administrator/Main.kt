e3f9e8ce07ed6ca42496fbcfb8afa88a566803c5
ElisaMin <xm.zhongjiaxin@gmail.com>
2020-12-10 17:45:00 +0800

    初次生成

---
+++ app/src/main/java/org/yanzuwu/live/administrator/Main.kt
@@ -0,0 +1,11 @@
+package org.yanzuwu.live.administrator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
