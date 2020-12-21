1a8f8a18dc638a0ff031fb03122c43554d78b409
ElisaMin <xm.zhongjiaxin@gmail.com>
2020-12-21 22:03:53 +0800

    我也没看

--- app/src/main/java/org/yanzuwu/live/administrator/data/beans/Result.kt
+++ app/src/main/java/org/yanzuwu/live/administrator/data/beans/Result.kt
@@ -3,19 +3,37 @@ package org.yanzuwu.live.administrator.data.beans
 /**
  * Result 结果表单
  *
- * @property isDone
- * @constructor Create empty Result
+ * @property type 表单类型
+ * @property isDone 任务是否完成
+ * @constructor 封闭类
  */
 sealed class Result (
+    open val type:String,
     open var isDone:Boolean=false
 ) {
-    class NotDoneYet():Result(false)
+    /**
+     * Not done yet 翻译:然而并没有完成
+     *
+     * @constructor Create a result
+     */
+    class NotDoneYet():Result("NotDone",false)
+
+    /**
+     * Fix devices task result 修理设备返回表单
+     * @property isDone 是否完成
+     * @property reason 造成设备损坏的原因
+     * @property theWayThatFixIt 维修方法
+     * @property isSomethingReplaced 是否替换了某些东西
+     * @property replacedDeviceInfo [Map]类型 替换信息
+     * @property images 图片
+     * @constructor Create empty Fix devices task result
+     */
     data class FixDevicesTaskResult (
         override var isDone : Boolean,
         val reason:String?=null,
-        val theWayYouFixIt:String?=null,
+        val theWayThatFixIt:String?=null,
         val isSomethingReplaced:Boolean?=null,
         val replacedDeviceInfo:Map<String,Int>?=null,
         val images:List<String>?=null
-    ): Result(isDone)
+    ): Result("fixDevice",isDone)
 }
--- app/src/main/java/org/yanzuwu/live/administrator/utils/FlowAdapter.kt
+++ app/src/main/java/org/yanzuwu/live/administrator/utils/FlowAdapter.kt
@@ -3,6 +3,8 @@ package org.yanzuwu.live.administrator.utils
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.CallSuper
+import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +13,21 @@ import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.flow.*
 import org.yanzuwu.live.administrator.data.beans.Task
+
+/**
+ * 适配Flow类型的RecyclerViewAdapter
+ * 可以重写onCollect方法
+ *
+ * @param T 该FLOW的类型
+ * @param B 不需要使用ViewHolder 只需要DataBinding
+ * @param flow
+ * @param layout return 一个LayoutResource
+ * @param onBind
+ * @param scope 需要[CoroutineScope]
+ * @param filter
+ * @constructor 封闭类 现在不是 过段时间就是了
+ */
+
 
 open class FlowAdapter <T,B:ViewDataBinding> constructor(
         val flow:Flow<T>,
@@ -24,21 +41,46 @@ open class FlowAdapter <T,B:ViewDataBinding> constructor(
     override fun getItemCount() : Int = count
     private val buffer:ArrayList<T> = ArrayList()
 
+
+
+
     init {
-        scope.launch(Main) {
-            flow.
-            filter { filter(it) }.
-            collect {
-                buffer.add(0,it)
-                notifyItemChanged(0)
-                count++
-            }
+        scope.launch(IO) {
+            flow.collect (::onCollect)
         }
     }
 
     class ViewHolder <B:ViewDataBinding> (val binding : B):RecyclerView.ViewHolder(binding.root)
+
+    companion object {
+        operator fun <T> invoke(
+                flow : Flow<T>,
+                layout:FlowAdapter<T,ViewDataBinding>.() -> Int,
+                scope : CoroutineScope,
+                onBind:ViewDataBinding.(T)->Unit,
+        ):FlowAdapter<T,ViewDataBinding> = FlowAdapter(
+                flow = flow,
+                layout=layout,
+                scope = scope,
+                onBind = onBind
+        )
+    }
+
+
+    @CallSuper
+    fun onCollect(it:T) {
+        if (filter(it)) {
+            buffer.add(0,it)
+            scope.launch(Main) {
+                notifyItemChanged(0)
+            }
+            count++
+        }
+    }
     override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : ViewHolder<B> =
         ViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context),layout(),parent,false))
+
+    @CallSuper
     override fun onBindViewHolder(holder : ViewHolder<B>, position : Int) {
         scope.launch (Main){
             holder.binding.onBind(buffer[position])
